b3ca5fe77ee96bd42e968f622bf0e353f399f21c
Navid Hariri <navidhariri25@gmail.com>
2020-04-30 01:56:09 -0700

    fixed the spacing issues with es lint on functional-timer.js

--- functional-timer.js
+++ functional-timer.js
@@ -6,12 +6,12 @@ let ID;
 /* sets the amount of thisTime and adds 1 to it
 for the user to see that it started from 30 and not 29 */
 function setTimer(timer) {
-    thisTime = timer + 1;
+  thisTime = timer + 1;
 }
 
 // sets Id
 function setID(idGiven) {
-    ID = idGiven;
+  ID = idGiven;
 }
 
 /* uses the id from global var
@@ -20,19 +20,19 @@ function setID(idGiven) {
  once it hits 0 it stops */
 
 function timeIt() {
-    thisTime--;
-    const timer = document.getElementById(ID);
-    timer.innerText = thisTime.toString();
+  thisTime--;
+  const timer = document.getElementById(ID);
+  timer.innerText = thisTime.toString();
 
-    if (thisTime === 0) {
-        clearInterval(theInterval);
-    }
+  if (thisTime === 0) {
+    clearInterval(theInterval);
+  }
 }
 
 function stopAndClear() {
-    const currentTimer = document.getElementById(ID);
-    currentTimer.innerText = '';
-    clearInterval(theInterval);
+  const currentTimer = document.getElementById(ID);
+  currentTimer.innerText = '';
+  clearInterval(theInterval);
 }
 
 
@@ -42,37 +42,38 @@ takes the ID that the developer wants to put the timer inside of
 when the timer hits 0 it stops itself */
 
 function startTimer(amount, id = 'timer') {
-    setTimer(amount);
-    setID(id);
-    timeIt();
-    theInterval = setInterval(timeIt, 1000);
+  setTimer(amount);
+  setID(id);
+  timeIt();
+  theInterval = setInterval(timeIt, 1000);
 }
 
 // converts seconds to minutes. For example 65 seconds to 1:05
 function convertSeconds(theSeconds) {
-    const min = Math.floor(theSeconds / 60);
-    const seconds = theSeconds % 60;
+  const min = Math.floor(theSeconds / 60);
+  const seconds = theSeconds % 60;
 
-    if (min > 1 && seconds < 10) {
-        return min + ':' + '0' + seconds;
-    } else {
-        return seconds;
-    }
+  if (min > 1 && seconds < 10) {
+    return min + ':' + '0' + seconds;
+  } else {
+    return seconds;
+  }
 
-    // else if (min > 1 && seconds ) {
-    //     return min + ':' + seconds;
-    // }
+  // else if (min > 1 && seconds ) {
+  //     return min + ':' + seconds;
+  // }
 }
 
 /* pauses the clock on the second that its said
-checks every second to see if its at the pausing point and once it is it clears the interval */
+checks every second to see if its at the pausing point
+and once it is it clears the interval */
 
 function pause(pointToPause) {
-    setInterval(function () {
-        pausing(pointToPause);
-    }, 1000);
+  setInterval(function() {
+    pausing(pointToPause);
+  }, 1000);
 
-    if (thisTime === pointToPause) {
-        clearInterval(theInterval);
-    }
+  if (thisTime === pointToPause) {
+    clearInterval(theInterval);
+  }
 }
